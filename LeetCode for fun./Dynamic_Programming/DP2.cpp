36a408e3ae4731049522d1cb9645acc245402753
YaHi <ghuy36603@gmail.com>
2025-07-22 07:23:39 +0700

    Create DP2.cpp

---
+++ LeetCode for fun./Dynamic_Programming/DP2.cpp
@@ -0,0 +1,24 @@
+// Frog Jump II
+#include <bits/stdc++.h>
+using namespace std;
+
+int DP[10000];
+int n, k;
+
+int solve_DP2(int k) {
+    DP[0] = 0;
+    for (int i = 1; i < n; i++) {
+        DP[i] = INT_MAX;
+        for (int j = max(0, i - k); j < i; j++) {
+            DP[i] = min(DP[j] + abs(A[i] - A[j]), DP[i]);
+        }
+    }
+    return DP[n - 1];
+}
+
+int main() {
+  cin >> n >> k;
+  cout << solve_DP2(k);
+  return 0;
+}
+  
