abfe5abd90dfbb6736205944cd5c6279bed2412f
YaHi <ghuy36603@gmail.com>
2025-07-23 20:34:52 +0700

    Create knapsack.cpp

---
+++ LeetCode for fun./Dynamic_Programming/knapsack.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int knapsack(int n, int W, int weight[], int value[]) {
+    DP[101][1001] = {0};
+    for (int i = 1; i <= n; i++) {
+        for (int w = 0; w <= W; w++) {
+            DP[i][w] = DP[i - 1][w];
+            if (w >= weight[i - 1]) {
+                DP[i][w] = max(DP[i][w], DP[i][w - weight[i - 1]] + value[i - 1]);
+            }
+        }
+    }
+    return DP[n][w];
+}
+
+int main() {
+    int n, W;
+    cin >> n >> W;
+    int weight[n], value[n];
+    for (int i = 0; i < n; i++) {
+        cin >> weight[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> value[i];
+    }
+    cout << knapsack(n, W, weight, value);
+    return 0;
+}
