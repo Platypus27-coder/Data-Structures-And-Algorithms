b9736364f29978738c3a0293db69f1279a3d8667
YaHi <ghuy36603@gmail.com>
2025-08-15 12:39:38 +0700

    Create ArticulationPoint.cpp

---
+++ LeetCode for fun./Depth-First Search./ArticulationPoint.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+vector<int> adj[1005], t_adj[1005];
+int visited[1005];
+stack<int> st;
+int timer = 0, scc = 0;
+int in_stack[1005];
+int disc[1005], low[1005];
+int AP[1005];
+
+void build(int n, int m) {
+    for (int i = 1; i <= n; i++) {
+        int x, y; 
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+}
+
+void dfs(int u, int par) {
+    visited[u] = 1;
+    disc[u] = low[u] = ++timer;
+    int child = 0;
+    for (int v : adj[u]) {
+        if (!visited[v]) {
+            dfs(v, u);
+            ++child;
+            low[u] = min(low[u], low[v]);
+            if (par != -1 && low[v] >= disc[u]) {
+                AP[u] = 1;
+            }
+        }
+        else {
+            low[u] = min(low[u], disc[v]);
+        }
+        if (par == -1 && child > 1) {
+            AP[u] = 1;
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> m >> n;
+    build(n, m);
+    for (int i = 1; i <= m; i++) {
+        if (!visited[i]) {
+            dfs(i, -1);
+        }
+    }
+    for (int i = 1; i <= m; i++) {
+        if (AP[i]) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
