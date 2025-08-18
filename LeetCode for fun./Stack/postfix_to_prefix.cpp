e351ffd0d449d8b6b06e21a119c48bb023361f13
YaHi <ghuy36603@gmail.com>
2025-08-18 17:26:28 +0700

    Create postfix_to_prefix.cpp

---
+++ LeetCode for fun./Stack/postfix_to_prefix.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <vector>
+#include<cctype>
+
+using namespace std;
+using ll = long long;
+
+void solve(string s) {
+    stack<string> st;   
+    for (int i = 0; i < s.size(); i++) {
+        if (isalpha(s[i])) {
+            st.push(string(1, s[i]));
+        }
+        else {
+            string op1 = st.top();
+            st.pop();
+            string op2 = st.top();
+            st.pop();
+            string tmp = string(1, s[i]) + op2 + op1;
+            st.push(tmp);
+        }
+    }
+    cout << st.top () << endl;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    solve(s);
+    return 0;
+}
