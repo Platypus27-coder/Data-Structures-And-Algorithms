b396b3a3aa18646f4c88f27c36b50ae6fd416066
YaHi <ghuy36603@gmail.com>
2025-08-19 15:10:10 +0700

    Create inOrder.cpp

---
+++ LeetCode for fun./Binary_Tree/inOrder.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#include <vector>
+#include<cctype>
+
+using namespace std;
+using ll = long long;
+
+struct node {
+    int data;
+    node *left;
+    node *right;
+};
+
+node *makeNode(int data) {
+    node *newNode = new node;
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+void makeRoot(node *root, int u, int v, char c) {
+    if (c == 'L') root->left = makeNode(v);
+    else root->right = makeNode(v);
+}
+
+void insertNode(node *root, int u, int v, char c) {
+    if (root == NULL) return;
+    if (root->data == u) {
+        makeRoot(root, u, v, c);
+    }
+    else {
+        insertNode(root->left, u, v, c);
+        insertNode(root->right, u, v, c);
+    }
+}
+
+void inOrder(node *root) {
+    if (root == NULL) return;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+int main() {
+    node *root = NULL;
+    int n; cin >> n;
+    for (int i = 0; i < n; i++) {
+        int u, v; char c;
+        cin >> u >> v >> c;
+        if (root == NULL) {
+            root = makeNode(u);
+            makeRoot(root, u, v, c);
+        }
+        else {
+            insertNode(root, u, v, c);
+        }
+    }
+    inOrder(root);
+    return 0;
+}
